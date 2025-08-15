be33d2d74c7f039e2aa6297f171afc4b421fac48
Luigi Erucci <erucciluigi@gmail.com>
2025-08-15 16:02:39 -0300

    Hotifx

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,18 @@
 import './styles/variables.scss';
 
 // UI
-export { default as Decal } from './components/ui/decal/decal';
 export { default as Title } from './components/ui/title/title';
-export { default as Stacks } from './components/ui/stacks/stacks';
 export { default as Button } from './components/ui/button/button';
 export { default as Subtitle } from './components/ui/subtitle/subtitle';
 export { default as Accordion } from './components/ui/accordion/accordion';
 export { default as TagLabel } from './components/ui/tag-label/tag-label';
-export { default as ThreeDots } from './components/ui/three-dots/three-dots';
-export { default as ProgressBar } from './components/ui/progress-bar/progress-bar';
-export { default as PulseEffect } from './components/ui/pulse-effect/pulse-effect';
 export { default as IconWithBackground } from './components/ui/icon-with-background/icon-with-background';
 
 // Shared
-export { default as Phone } from './components/shared/phone/phone';
-export { default as AutoPlayer } from './components/shared/autoplayer/autoplayer';
 export { default as InfoHeader } from './components/shared/info-header/info-header';
-export { default as IndexHeader } from './components/shared/index-header/index-header';
-export { default as SuccessCases } from './components/shared/success-cases/success-cases';
-export { default as FooterBanner } from './components/shared/footer-banner/footer-banner';
 export { default as NewsletterStrip } from './components/shared/newsletter-strip/newsletter-strip';
 export { default as NewsletterModal } from './components/shared/newsletter-modal/newsletter-modal';
-export { default as SupportStickyButton } from './components/shared/support-sticky-button/support-sticky-button';
 export { default as NewsletterModalDialog } from './components/shared/newsletter-modal-dialog/newsletter-modal-dialog'
-export { default as FooterBannerAutoplayer } from './components/shared/footer-banner-autoplayer/footer-banner-autoplayer';
-export { default as SuccessCasesSwiper } from './components/shared/success-cases/success-cases-swiper/success-cases-swiper';
-export { default as SuccessCaseCard } from './components/shared/success-cases/success-cases-swiper/success-case-card/success-case-card'
 
 // Layout
 export { default as Footer } from './components/layout/footer/footer';
