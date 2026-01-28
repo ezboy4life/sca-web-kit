8cd72eb03edbef6f1ec5a45441a0a62022c4508d
Luigi Erucci <erucciluigi@gmail.com>
2026-01-28 16:59:05 -0300

    3.3.8 (Ajuste H1 -> H2)

--- src/components/ui/title/title.tsx
+++ src/components/ui/title/title.tsx
@@ -20,7 +20,7 @@ export default function Title(
   }
 ) {
   return (
-    <h1
+    <h2
       id={id}
       className={`${styles['title']} ${className} ${montserrat.className}`}
       style={{
@@ -30,6 +30,6 @@ export default function Title(
       }}
     >
       {text}
-    </h1>
+    </h2>
   )
 }
